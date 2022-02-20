4f2d5a0bb5cba9a3480362ed0a7b55d6795faf93
Leonardo2526 <leonardo147wot@gmail.com>
2022-02-20 19:18:54 +0300

    ListRemover added

---
+++ CollectionTest/Collections/ListRemover.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionTest.Collections
+{
+    class ListRemover : ICollectionCreator
+    {
+        public List<int> GetValues()
+        {
+            List<int> list = new List<int>();
+            for (int i = 3; i < 1000000; i++)
+            {
+                list.Add(i);
+            }
+
+            //Console.WriteLine("Initial queue: ");
+            //Print.PrintIndexAndValues(list);
+
+            int cnt = list.Count;
+            for (int i = 0; i < cnt - 1; i++)
+            {
+                list.RemoveAt(0);
+            }
+
+            return list;
+        }
+    }
+}
--- CollectionTest/Collections/QueueCollection.cs
+++ CollectionTest/Collections/QueueCollection.cs
@@ -10,36 +10,29 @@ namespace CollectionTest.Collections
     {
         public List<int> GetValues()
         {
-
             var queue = new Queue<int>();
 
-            for (int i = 3; i < 10; i++)
+            for (int i = 3; i < 1000000; i++)
             {
                 queue.Enqueue(i);
             }
 
-            Print.PrintIndexAndValues(queue);
+            //Console.WriteLine("Initial queue: ");
+            //Print.PrintIndexAndValues(queue);
 
             // получаем элемент из самого начала очереди 
             //var firstPerson = queue.Peek();
             //Console.WriteLine(firstPerson);
 
-            var item1 = queue.Dequeue();
-            Console.WriteLine(item1); 
-
-            Print.PrintIndexAndValues(queue);
-
-            List<int> outlist = new List<int>();
-            for (int i = 0; i < queue.Count; i++)
+            int cnt = queue.Count;
+            for (int i = 0; i < cnt - 1; i++)
             {
-                var item = queue.Dequeue();
-                outlist.Add(item);
+                queue.Dequeue();
             }
 
-
-
-            return outlist;
+            return queue.ToList();
 
         }
+       
     }
 }
