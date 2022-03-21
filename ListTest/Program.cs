cae1d62ab5cef23b257695199bc245f15cab19b0
Leonardo2526 <leonardo147wot@gmail.com>
2022-03-21 11:09:15 +0300

    ListTest added

---
+++ ListTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test1.EqualLists();
+
+            Console.WriteLine("List1");
+            foreach (var item in Test1.List1)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("\nList2");
+
+            foreach (var item in Test1.List2)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ListTest/Test1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListTest
+{
+    static class Test1
+    {
+        public static List<int> List1 { get; private set; } = new List<int>()
+            {
+                1,2,3
+            };
+
+        public static List<int> List2 { get; } = new List<int>()
+            {
+                4,5
+            };
+
+
+        public static void EqualLists()
+        {
+            List1 = List2;
+        }
+
+    }
+}
