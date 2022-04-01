796cc2e757f23b39f4541d508776d15efa56a3b2
Leonardo2526 <leonardo147wot@gmail.com>
2022-04-01 09:57:58 +0300

    Compare added

---
+++ DS.ConsoleApp.Compare/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS.ConsoleApp.Compare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A1 = 10;
+            double A2 = -10;
+            double A3 = 2;
+
+
+            double B1 = 2;
+            ;
+            Console.WriteLine(A1.CompareTo(B1));
+            Console.WriteLine(A2.CompareTo(B1));
+            Console.WriteLine(A3.CompareTo(B1));
+            Console.WriteLine(A1.CompareTo(B1.CompareTo((Object)B1)));
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
