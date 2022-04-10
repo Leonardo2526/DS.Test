87713b977878fbbca86a31060c8626a0c92eab1e
Leonardo2526 <leonardo147wot@gmail.com>
2022-04-10 21:17:39 +0300

    CollisionBuilder added

---
+++ Builder/Collision/BypassModelBuilder.cs
@@ -0,0 +1,20 @@
+﻿namespace Builder
+{
+    class BypassModelBuilder : CollisionBuilder
+    {
+        protected BypassModel BypassModel { get; set; } = new BypassModel();
+
+        public BypassModelBuilder(Collision collision)
+        {
+            this.Collision = collision;
+            Collision.BypassModel = BypassModel;
+        }
+
+        public BypassModelBuilder NameOf(string name)
+        {
+            BypassModel.NameBypass = name;
+            return this;
+        }
+
+    }
+}
