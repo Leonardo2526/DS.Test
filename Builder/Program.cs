b579b7aa1393f431b66514b3a0e14394de9fec78
Leonardo2526 <leonardo147wot@gmail.com>
2022-04-10 17:04:41 +0300

    Builder added

---
+++ Builder/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StepwiseBuilder;
+
+namespace Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = CarBuilder.Create()
+              .OfType(CarType.Crossover)
+              .WithWheels(18)
+              .Build();
+            Console.WriteLine(car);
+
+            Console.ReadLine();
+        }
+    }
+}
