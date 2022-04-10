8fa7b9fc64b67a43ad4759558a1ffbc757827086
Leonardo2526 <leonardo147wot@gmail.com>
2022-04-10 17:43:17 +0300

    BuilderFacets added

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BuilderFacets;
 using StepwiseBuilder;
 
 namespace Builder
@@ -11,13 +12,39 @@ namespace Builder
     {
         static void Main(string[] args)
         {
-            var car = CarBuilder.Create()
-              .OfType(CarType.Crossover)
-              .WithWheels(18)
-              .Build();
-            Console.WriteLine(car);
+            //RunCarBuilder();
+            RunPersonBuilder();
+         
 
             Console.ReadLine();
         }
+
+        private static void RunPersonBuilder()
+        {
+            var pb = new PersonBuilder();
+            Person person = pb
+              .Lives
+                .At("123 London Road")
+                .In("London")
+                .WithPostcode("SW12BC")
+              .Works
+                .At("Fabrikam")
+                .AsA("Engineer")
+                .Earning(123000);
+
+            Console.WriteLine(person);
+        }
+
+        private static void RunCarBuilder()
+        {
+            var car = CarBuilder.Create()
+           .OfType(CarType.Crossover)
+           .WithWheels(18)
+           .Build();
+            Console.WriteLine(car.Type);
+            Console.WriteLine(car.WheelSize);
+        }
+
+
     }
 }
