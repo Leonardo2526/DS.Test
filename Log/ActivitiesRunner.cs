bdb9a18ee4c4f75a7613efad428c8d65b042bbf2
Leonardo2526 <leonardo147wot@gmail.com>
2022-04-22 13:30:06 +0300

    Tracing added

---
+++ Log/ActivitiesRunner.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tracing
+{
+    class ActivitiesRunner
+    {
+        private AbstractActivity Activity;
+
+        public ActivitiesRunner(AbstractActivity activity)
+        {
+            this.Activity = activity;
+        }
+
+        public void Run()
+        {
+            this.Activity.Create();
+            this.Activity.TS.Close();
+        }
+    }
+}
