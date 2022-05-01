3646028e332a6c3d8af746dba5faf03290e92c4a
Danil <leonardo147wot@gmail.com>
2022-05-02 01:06:32 +0300

    Phones MVVM added

---
+++ WPF/DS.WpfApp.Phones.MVVM/UserControl1.xaml.cs
@@ -0,0 +1,17 @@
+﻿using DS.WpfApp.Phones.MVVM.ViewModel;
+using System.Windows;
+
+namespace DS.WpfApp.Phones.MVVM
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : Window
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+            DataContext = new ApplicationViewModel();
+        }
+    }
+}
