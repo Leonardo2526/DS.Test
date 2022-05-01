10da33628f425b930757474adffe327c6f759525
Danil <leonardo147wot@gmail.com>
2022-05-01 23:43:23 +0300

    new projects added

---
+++ WPF/DS.WpfApp.Phones.Resources/BindingApp.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class BindingApp : Window
+    {
+        public Phone Phone { get; set; } = new Phone();
+
+        public BindingApp()
+        {
+            InitializeComponent();
+            AddNew();
+        }
+
+        void AddNew()
+        {
+            Phone phone = (Phone)this.Resources["nexusPhone"];
+            phone.Company = "LG"; // Меняем с Google на LG
+        }
+    }
+}
