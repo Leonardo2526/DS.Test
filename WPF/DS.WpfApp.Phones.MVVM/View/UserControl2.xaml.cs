e4a874ec4050088eb848e107a54ea65ea5e0946f
Danil <leonardo147wot@gmail.com>
2022-05-02 01:19:54 +0300

    sorted by folders

--- WPF/DS.WpfApp.Phones.MVVM/View/UserControl2.xaml.cs
+++ WPF/DS.WpfApp.Phones.MVVM/View/UserControl2.xaml.cs
@@ -1,17 +1,18 @@
 ﻿using DS.WpfApp.Phones.MVVM.ViewModel;
 using System.Windows;
 
-namespace DS.WpfApp.Phones.MVVM
+namespace DS.WpfApp.Phones.MVVM.View
 {
     /// <summary>
-    /// Interaction logic for UserControl1.xaml
+    /// Interaction logic for UserControl2.xaml
     /// </summary>
-    public partial class UserControl1 : Window
+    public partial class UserControl2 : Window
     {
-        public UserControl1()
+        public UserControl2()
         {
             InitializeComponent();
             DataContext = new ApplicationViewModel();
+
         }
     }
 }
