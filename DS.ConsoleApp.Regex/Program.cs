b7807afe37fcd7bd4a635d8b4b8b8c283103b62a
Leonardo2526 <leonardo147wot@gmail.com>
2022-05-05 18:42:24 +0300

    regex checker added

---
+++ DS.ConsoleApp.Regex/Program.cs
@@ -0,0 +1,22 @@
+﻿using DS.MainUtils.Strings;
+using DS.MainUtils;
+
+
+var value = "60";
+//if (value.IsDevisible(10))
+if (value.IsInt())
+{
+    Console.WriteLine($"{value} is valid.");
+}
+else
+{
+    Console.WriteLine($"{value} is not valid.");
+}
+
+Console.ReadLine();
+
+double newValue = int.Parse(value);
+//double newValue = double.Parse(value);
+Console.WriteLine(newValue);
+
+
