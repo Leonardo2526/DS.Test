ccca39600649ac2b7ba7b429a2d2002e8131d6e7
Leonardo2526 <leonardo147wot@gmail.com>
2022-05-05 19:05:14 +0300

    AgeValidator added

---
+++ DS.WpfApp.PersonValidation2/AgeValidator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using DS.MainUtils.Strings;
+using DS.MainUtils;
+
+namespace DS.WpfApp.PersonValidation2
+{
+    public class AgeValidator : ValidationRule
+    {
+        public override ValidationResult Validate
+          (object value, System.Globalization.CultureInfo cultureInfo)
+        {
+            string? stringValue = value as string;
+
+            if (String.IsNullOrEmpty(stringValue))
+            {
+                return new ValidationResult(false, "Значение не может быть нулевым.");
+            }
+            else if (!stringValue.IsInt())
+            {
+                return new ValidationResult(false, "Значение должно быть целым числом.");
+            }
+
+            int MaxValue = 1000;
+            int MinValue = 10;
+
+            int.TryParse(stringValue, out int intValue);
+            if (!intValue.IsDevisible(10))
+            {
+                return new ValidationResult(false, "Значение должно быть кратным 10.");
+            }
+            else if (intValue < 10 || intValue > MaxValue)
+            {
+                return new ValidationResult(false, $"Значение должно быть в пределах от {MinValue} до {MaxValue}.");
+            }
+            return ValidationResult.ValidResult;
+        }
+    }
+}
