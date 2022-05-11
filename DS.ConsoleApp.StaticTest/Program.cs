c7f5b7e06d486bbd9a73073ff10abc6d21d5fc3b
Leonardo2526 <leonardo147wot@gmail.com>
2022-05-11 13:11:12 +0300

    StaticTest added

---
+++ DS.ConsoleApp.StaticTest/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DS.ConsoleApp.StaticTest;
+
+Class1 class1 = new Class1();
+
+class1.Operation();
+Console.WriteLine(class1.MyProperty);
+class1.Operation();
+Console.WriteLine(class1.MyProperty);
+class1.Operation();
+Console.WriteLine(class1.MyProperty);
+
+Console.ReadLine();
