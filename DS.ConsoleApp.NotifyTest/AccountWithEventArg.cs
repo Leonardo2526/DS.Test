a41892448ed547581ed900e1b6bc3ceeb366d7cb
Leonardo2526 <leonardo147wot@gmail.com>
2022-05-15 14:25:16 +0300

    AccountEventArgs added

---
+++ DS.ConsoleApp.NotifyTest/AccountWithEventArg.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS.ConsoleApp.NotifyTest
+{
+    internal class AccountWithEventArg
+    {
+        public delegate void AccountHandler(AccountWithEventArg sender, AccountEventArgs e);
+        public event AccountHandler? Notify;
+
+        public int Sum { get; private set; }
+
+        public AccountWithEventArg(int sum) => Sum = sum;
+
+        public void Put(int sum)
+        {
+            Sum += sum;
+            Notify?.Invoke(this, new AccountEventArgs($"На счет поступило {sum}", sum));
+        }
+        public void Take(int sum)
+        {
+            if (Sum >= sum)
+            {
+                Sum -= sum;
+                Notify?.Invoke(this, new AccountEventArgs($"Сумма {sum} снята со счета", sum));
+            }
+            else
+            {
+                Notify?.Invoke(this, new AccountEventArgs("Недостаточно денег на счете", sum));
+            }
+        }
+    }
+}
--- DS.ConsoleApp.NotifyTest/Program.cs
+++ DS.ConsoleApp.NotifyTest/Program.cs
@@ -1,18 +1,5 @@
 ﻿using DS.ConsoleApp.NotifyTest;
 
-Account account = new Account(100);
-account.Notify += DisplayMessage;       // добавляем обработчик DisplayMessage
-account.Notify += DisplayRedMessage;    // добавляем обработчик DisplayMessage
-account.Put(20);    // добавляем на счет 20
-account.Notify -= DisplayRedMessage;     // удаляем обработчик DisplayRedMessage
-account.Put(50);    // добавляем на счет 50
-
-void DisplayMessage(string message) => Console.WriteLine(message);
-void DisplayRedMessage(string message)
-{
-    // Устанавливаем красный цвет символов
-    Console.ForegroundColor = ConsoleColor.Red;
-    Console.WriteLine(message);
-    // Сбрасываем настройки цвета
-    Console.ResetColor();
-}
+//AccountOutput.CallAccountAdd();
+//AccountOutput.AddRemove();
+AccountOutput.CallEventArg();
