917aab752d04a79dcc8415a01a695cb28a7b96ce
Leonardo2526 <leonardo147wot@gmail.com>
2022-05-21 21:35:21 +0300

    CollectionTest added

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,5 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using DS.ConsoleApp.TestingObjects;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TestProject1
 {
@@ -33,6 +35,18 @@ namespace TestProject1
         public void AddTests2(int x, int y, int expected)
         {
             Assert.AreEqual(expected, x + y);
+        }
+
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetTestData1), DynamicDataSourceType.Method)]
+        public void CollectionTest(IEnumerable<int> list1)
+        {
+            CollectionAssert.AllItemsAreNotNull((System.Collections.ICollection)list1);
+        }
+        public static IEnumerable<object[]> GetTestData1()
+        {
+            yield return new object[] { new List<int>() { 1 } };
         }
     }
 }
