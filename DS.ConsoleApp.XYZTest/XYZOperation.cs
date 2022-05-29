66a5049f7dbe470a1777c96d57bc7b263ea0891d
Danil <leonardo147wot@gmail.com>
2022-05-30 00:52:22 +0300

    new methods and tests added

--- DS.ConsoleApp.XYZTest/XYZOperation.cs
+++ DS.ConsoleApp.XYZTest/XYZOperation.cs
@@ -21,7 +21,7 @@ namespace DS.ConsoleApp.XYZTest
             double[,] matrix = CreateMatrix3D(vector1, vector2, vector3);
             double det = Matrix.GetMatrixDeterminant(matrix);
 
-            if(det > 0)
+            if (det > 0)
             {
                 return true;
             }
@@ -49,15 +49,15 @@ namespace DS.ConsoleApp.XYZTest
         /// <param name="vector2"></param>
         /// <param name="vector3"></param>
         /// <returns>Return 3D matrix.</returns>
-        private static double [,] CreateMatrix3D(XYZ vector1, XYZ vector2, XYZ vector3)
+        private static double[,] CreateMatrix3D(XYZ vector1, XYZ vector2, XYZ vector3)
         {
             List<XYZ> result = new List<XYZ>()
             {
                 vector1, vector2, vector3
             };
 
-            double[,] matrix = new double[3,3];
-            
+            double[,] matrix = new double[3, 3];
+
             for (int i = 0; i < 3; i++)
             {
                 matrix[i, 0] = result[i].X;
@@ -67,6 +67,84 @@ namespace DS.ConsoleApp.XYZTest
 
             return matrix;
         }
-        
+
+        /// <summary>
+        /// Get cross product(vector product) for right hand side cartesian coordinate system
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public static XYZ GetCrossProduct(XYZ a, XYZ b)
+        {
+            return new XYZ(a.Y * b.Z - a.Z * b.Y, a.Z * b.X - a.X * b.Z, a.X * b.Y - a.Y * b.X);
+        }
+
+        /// <summary>
+        /// Get dot product or scalar product of two vectors.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static double GetDotProduct(XYZ a, XYZ b)
+        {
+            return a.X * b.X + a.Y * b.Y + a.Z * b.Z;
+        }
+
+        /// <summary>
+        /// Get tripple product for right hand side cartesian coordinate system
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public static double GetTrippleProduct(XYZ a, XYZ b, XYZ c)
+        {
+            return GetDeterminantByVectors(a, b, c);
+        }
+
+
+        /// <summary>
+        /// Get length of vector.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public static double GetLength(XYZ a)
+        {
+            return Math.Sqrt(GetDotProduct(a, a));
+        }
+
+        /// <summary>
+        /// Check if two vectors are colliner.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns>Return true if two vectors are colliner.</returns>
+        public static bool Collinearity(XYZ a, XYZ b)
+        {
+            XYZ crossProduct = GetCrossProduct(a, b);
+            if (GetLength(crossProduct) == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Check if two vectors are coplanar.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns>Return true if two vectors are coplanar.</returns>
+        public static bool Coplanarity(XYZ a, XYZ b, XYZ c)
+        {
+            if(GetTrippleProduct(a, b, c) == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
     }
 }
---
+++ TestProject1/XYZCrossProductTest.cs
@@ -0,0 +1,40 @@
+﻿using DS.ConsoleApp.XYZTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS.TestProject.VS
+{
+    [TestClass]
+    public  class XYZCrossProductTest
+    {
+        [DataTestMethod]
+        [DynamicData(nameof(GetBasis), DynamicDataSourceType.Method)]
+        public void XYZOrientationTest(XYZ vector1, XYZ vector2, XYZ crossProduct)
+        {
+            Assert.AreEqual(crossProduct, XYZOperation.GetCrossProduct(vector1, vector2));
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetFailBasis), DynamicDataSourceType.Method)]
+        public void XYZOrientationFailTest(XYZ vector1, XYZ vector2, XYZ crossProduct)
+        {
+            Assert.AreNotEqual(crossProduct, XYZOperation.GetCrossProduct(vector1, vector2));
+        }
+
+        public static IEnumerable<object[]> GetBasis()
+        {
+            yield return new object[] { new XYZ(1, 0, 0), new XYZ(0, 1, 0), new XYZ(0, 0, 1) };
+            yield return new object[] { new XYZ(1, 0, 0), new XYZ(0, -1, 0), new XYZ(0, 0, -1) };
+        }
+
+        public static IEnumerable<object[]> GetFailBasis()
+        {
+            yield return new object[] { new XYZ(1, 0, 0), new XYZ(0, 1, 0), new XYZ(0, 0, 10) };
+            yield return new object[] { new XYZ(1, 0, 0), new XYZ(0, -1, 0), new XYZ(10, 0, -1) };
+        }
+    }
+}
