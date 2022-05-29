0b4ca1b0c1f55b25d8214c69a75c7905867c0148
Leonardo2526 <leonardo147wot@gmail.com>
2022-05-29 19:19:17 +0300

    uni test for XYZ added

---
+++ TestProject1/XYZOperationTest.cs
@@ -0,0 +1,27 @@
+﻿using DS.ConsoleApp.XYZTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS.TestProject.VS
+{
+    [TestClass]
+    internal class XYZOperationTest
+    {
+        [DataTestMethod]
+        [DynamicData(nameof(GetTestData1), DynamicDataSourceType.Method)]
+        public void SumTests(IEnumerable<double> list1)
+        {
+            //XYZOperation.Is3DOrientationEqualToOrigin(list1.GetEnumerator());
+        }
+
+        public static IEnumerable<object[]> GetTestData1()
+        {
+            yield return new object[] { new double[1, 0, 0], new double[0, 1, 0], new double[0, 0, 1]};
+        }
+      
+    }
+}
