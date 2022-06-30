d334db40a012fa6c9fa26801a2db81eb269e08da
Leonardo2526 <leonardo147wot@gmail.com>
2022-06-30 19:04:18 +0300

    PrintAwait added

---
+++ Async/Async/NamePrinter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Async
+{
+    internal static class NamePrinter
+    {
+        public static async Task PrintAwait()
+        {
+            await PrintNameAsync("Tom");
+            await PrintNameAsync("Bob");
+            await PrintNameAsync("Sam");
+        }
+
+        public static async Task PrintNoAwait()
+        {
+            var tomTask = PrintNameAsync("Tom");
+            var bobTask = PrintNameAsync("Bob");
+            var samTask = PrintNameAsync("Sam");
+
+            await tomTask;
+            await bobTask;
+            await samTask;
+        }
+
+        // определение асинхронного метода
+        static async Task PrintNameAsync(string name)
+        {
+            await Task.Delay(3000);     // имитация продолжительной работы
+            Console.WriteLine(name);
+        }
+    }
+}
--- Async/Async/Program.cs
+++ Async/Async/Program.cs
@@ -9,45 +9,14 @@ namespace Async
 {
     class Program
     {
-        static void Main(string[] args)
+        async static Task Main(string[] args)
         {
-            string url = "https://www.dotnetfoundation.org";
-            int count = 500;
-
-            //Console.WriteLine(GetHtmlAsync(url).Result);
-            Console.WriteLine(GetFirstCharactersCountAsync(url, count).Result);
+            //HTMLLoader.Load();
+            //await NamePrinter.PrintAwait();
+            await NamePrinter.PrintNoAwait();
 
             Console.ReadLine();
         }
 
-        public static Task<string> GetHtmlAsync(string url)
-        {
-            // Execution is synchronous here
-            var client = new HttpClient();
-
-            return client.GetStringAsync(url);
-        }
-
-        public static async Task<string> GetFirstCharactersCountAsync(string url, int count)
-        {
-            // Execution is synchronous here
-            var client = new HttpClient();
-
-            // Execution of GetFirstCharactersCountAsync() is yielded to the caller here
-            // GetStringAsync returns a Task<string>, which is *awaited*
-            var page = await client.GetStringAsync("https://www.dotnetfoundation.org");
-
-            // Execution resumes when the client.GetStringAsync task completes,
-            // becoming synchronous again.
-
-            if (count > page.Length)
-            {
-                return page;
-            }
-            else
-            {
-                return page.Substring(0, count);
-            }
-        }
     }
 }
