76a41fa33a137944db872fe631dc3957d86b3f6e
Leonardo2526 <leonardo147wot@gmail.com>
2022-07-17 00:35:58 +0300

    ClientCast added

---
+++ CollectionTest/CastTest/ConreteClass2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionTest.CastTest
+{
+    internal class ConcreteClass2 : AbstractClass
+    {
+        public int Prop2 { get; set; }
+
+    }
+}
--- CollectionTest/Program.cs
+++ CollectionTest/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Autofac;
+using CollectionTest.CastTest;
 using System;
 using System.Collections.Generic;
 
@@ -6,27 +7,29 @@ namespace CollectionTest
 {
     public class Program
     {
-        public static List<int> Run()
-        {
-            var container = ContainerConfig.Configure();
+        //public static List<int> Run()
+        //{
+        //    var container = ContainerConfig.Configure();
 
-            using (var scope = container.BeginLifetimeScope())
-            {
-                var app = scope.Resolve<IApplication>();
+        //    using (var scope = container.BeginLifetimeScope())
+        //    {
+        //        var app = scope.Resolve<IApplication>();
 
-                return app.Run();
-            }
-        }
+        //        return app.Run();
+        //    }
+        //}
 
         static void Main(string[] args)
         {
-            List<int> outlist = Run();
+            ClientCast.RunTest();
+
+            //List<int> outlist = Run();
 
             //Console.WriteLine("Result list: ");
             //Print.PrintIndexAndValues(outlist);
 
 
-            Console.WriteLine("\nList count: " + outlist.Count);
+            //Console.WriteLine("\nList count: " + outlist.Count);
             Console.ReadLine();
         }
     }
