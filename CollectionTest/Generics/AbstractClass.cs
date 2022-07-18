57d411fd41978457ec9b9046d5d32b6df35c573e
Leonardo2526 <leonardo147wot@gmail.com>
2022-07-18 13:44:39 +0300

    Generics added

---
+++ CollectionTest/Generics/AbstractClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionTest.Generics
+{
+    internal abstract class AbstractClass<T>
+    {
+        public abstract T Name { get; set; }
+    }
+}
