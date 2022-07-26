a91ac9c5fad4beb4842db43355fe117a9ebd81aa
Danil <leonardo147wot@gmail.com>
2022-07-27 00:50:27 +0300

    PrintAllFields added

--- CollectionTest/Generics1/ClientGeneric1.cs
+++ CollectionTest/Generics1/ClientGeneric1.cs
@@ -62,7 +62,7 @@ namespace CollectionTest.Generics1
             var class1 = new Class1() { Name = "class1", MyElement = element1 };
 
             var element2 = new MEPCurve() { Name = "mepCurve" , MEPId = 2};
-            var class2 = new Class2();
+            var class2 = new Class2() { Name = "class2", MyElement = element2 };
 
             list.Add(class1);
             list.Add(class2);
@@ -73,12 +73,12 @@ namespace CollectionTest.Generics1
             //{
             //    Console.WriteLine(item.Name + " - " + item.MyElement.Name);
             //}
-
-            foreach (var item in list)
-            {
-                var baseItem = (BaseClass<BaseElement>)item;
-                Console.WriteLine(baseItem.Name + " - " + baseItem.MyElement.Name);
-            }
+            PrintAllFields(list);
+            //foreach (var item in list)
+            //{
+            //    var baseItem = (BaseClass<BaseElement>)item;
+            //    Console.WriteLine(baseItem.Name + " - " + baseItem.MyElement.Name);
+            //}
 
 
             Console.WriteLine("\n class1 types:");
@@ -99,6 +99,57 @@ namespace CollectionTest.Generics1
                 Console.WriteLine(item.Name + " - " + prop1 + " - " + prop2);
             }
         }
+
+        private static void PrintAllFields(List<IProperty> list)
+        {
+            List<BaseElement> baseElements = new List<BaseElement>();
+
+            //var elems = GetElements(class1Types);
+
+            var class1Types = list.OfType<Class1>();
+            foreach (var item in class1Types)
+            {
+                baseElements.Add(item.MyElement);
+            }
+
+            var class2Types = list.OfType<Class2>();
+            foreach (var item in class2Types)
+            {
+                baseElements.Add(item.MyElement);
+            }
+
+            foreach (var item in baseElements)
+            {
+                Console.WriteLine(item.Name);
+            }
+        }
+
+
+        private static List<BaseElement> GetElements(IEnumerable<BaseClass<BaseElement>> list)
+        {
+            List<BaseElement> baseElements = new List<BaseElement>();
+
+            foreach (var item in list)
+            {
+                baseElements.Add(item.MyElement);
+            }
+
+            return baseElements;
+        }
+
+        //private static void PrintAllFields(List<IProperty> list)
+        //{
+        //Type type = typeof(BaseClass<BaseElement>);
+        //ReflectiveEnumerator.GetEnumerableOfType<Type>();
+
+        ////Type type = list.First().GetType();
+        //var types = type.GEt
+        ////var types = type.GetNestedTypes();
+        //foreach (var item in types)
+        //{
+        //    Console.WriteLine(item.Name);
+        //}
+        //}
 
     }
 }
