03d63733e0fc1e1875e095499767c10b78c0f248
Leonardo2526 <leonardo147wot@gmail.com>
2022-08-04 13:20:14 +0300

    TryCatchFinallyTest added

---
+++ Misc/TryCatchFinallyTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Misc
+{
+    internal static class TryCatchFinallyTest
+    {
+        public static void Run()
+        {
+            try
+            {
+                try
+                {
+                    Console.WriteLine("Executing the try statement.");
+                    int x = 5;
+                    int y = x / 0;
+                    Console.WriteLine($"Результат: {y}");
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine("Возникло исключение!");
+                }
+                finally
+                {
+                    Console.WriteLine("Блок finally");
+                }
+
+                Console.WriteLine("End of the try statement");
+            }
+
+            catch { }
+
+            Console.WriteLine("Конец программы");
+            Console.ReadKey();
+        }
+    }
+}
