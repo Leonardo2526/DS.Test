1f1644b44dc36aeb31e348737060167acf68d9ba
Leonardo2526 <leonardo147wot@gmail.com>
2022-08-19 17:32:16 +0300

    DoubleEqualsTest added

--- TestProject1/ObjectEqualTest.cs
+++ TestProject1/ObjectEqualTest.cs
@@ -3,6 +3,7 @@ using DS.ConsoleApp.TestingObjects;
 using System.Collections.Generic;
 using System.Linq;
 using DS.ConsoleApp.XYZTest;
+using System;
 
 namespace DS.TestProject.VS
 {
@@ -32,6 +33,19 @@ namespace DS.TestProject.VS
             Assert.IsTrue(eq);
         }
 
-      
+
+        [DataTestMethod]
+        [DataRow(1, 1, true)]
+        [DataRow(1, 1.1, false)]
+        [DataRow(1.1, 1.123, true)]
+
+        public void DoubleEqualsTest(double d1, double d2, bool eq)
+        {
+            bool res = d1.Equals(Math.Round(d2,1));
+
+            Assert.AreEqual(eq, res);
+        }
+
+
     }
 }
