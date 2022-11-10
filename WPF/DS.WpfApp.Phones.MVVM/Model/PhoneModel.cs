830bf2c77221033f476c0ed71e468d9df1b026e3
Данил Сараев <leonardo147wot@gmail.com>
2022-11-10 21:08:32 +0300

    PhoneModel updated

--- WPF/DS.WpfApp.Phones.MVVM/Model/PhoneModel.cs
+++ WPF/DS.WpfApp.Phones.MVVM/Model/PhoneModel.cs
@@ -1,15 +1,30 @@
 ﻿using DS.ClassLib.Models;
 using DS.ClassLib.VarUtils;
+using DS.ClassLib.VarUtils.Events;
+using System;
 using System.Windows;
 using System.Windows.Input;
 
 namespace DS.WpfApp.Phones.MVVM.Model
 {
-    public class PhoneModel : Phone
+    public class PhoneModel : Phone, IEvent<EventType>
     {
+
+        public PhoneModel()
+        {
+            Event += Handler;
+        }
+
+        public event EventHandler<EventType> Event;
         public ICommand Command1 => new RelayCommand(c =>
         {
-            MessageBox.Show(Title);
+            Event?.Invoke(this, EventType.Apply);
+            //MessageBox.Show(Title);
         });
+
+        void Handler(object s, EventType eventType)
+        {
+            MessageBox.Show(eventType.ToString());
+        }
     }
 }
