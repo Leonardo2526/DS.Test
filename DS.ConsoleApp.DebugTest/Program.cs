09498924ddad2fd5e634a90231c364debd01da6a
Данил Сараев <leonardo147wot@gmail.com>
2022-11-13 11:56:24 +0300

    DS.ConsoleApp.DebugTest added

---
+++ DS.ConsoleApp.DebugTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Specialized;
+using System.Configuration;
+using System.Diagnostics;
+
+NameValueCollection sAll;
+sAll = ConfigurationManager.AppSettings;
+foreach (string s in sAll.AllKeys)
+    Console.WriteLine("Key: " + s + " Value: " + sAll.Get(s));
+
+Debug.AutoFlush = true;
+//Debug.IndentSize= 50;
+Debug.IndentLevel= 0;
+Debug.Indent();
+Debug.WriteLine("Entering Main");
+Console.WriteLine("Hello World.");
+Debug.WriteLine("Exiting Main");
+Debug.Unindent();
+
+Debug.WriteLine("Unindent");
+
+Console.ReadLine();
