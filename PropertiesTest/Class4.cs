ac6f8e3d136048817d543b13288b54e177a2b28c
Данил Сараев <leonardo147wot@gmail.com>
2023-02-13 12:30:33 +0300

    props upd

---
+++ PropertiesTest/Class4.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesTest
+{
+    internal class Class4
+    {
+        public Class4()
+        {
+
+        }
+
+        //get value only once and save on it to get next
+        public int Prop1 { get; } = GetProp();
+
+        //get value only once and save on it to get next
+        public static int Prop2 { get; } = GetProp2();
+
+        //get value only once and save on it to get next
+        public int Prop3 { get; } = new Class1().MyProperty1;
+
+        //initiate get value every time when apply
+        public int Prop4
+
+        {
+            get
+            {
+                return GetProp();
+            }
+        }
+
+
+        private static int GetProp()
+        {
+            int i = 0;
+            return i;
+        }
+
+        private static int GetProp2()
+        {
+            int i = 1;
+            return i;
+        }
+    }
+}
--- PropertiesTest/Program.cs
+++ PropertiesTest/Program.cs
@@ -15,11 +15,25 @@ namespace PropertiesTest
 
             //Console.WriteLine(class1.MyProperty2);
 
-            Class2 class2 = new Class2();
-            class2.MyProperty = 1;
+            //Class2 class2 = new Class2();
+            //class2.MyProperty = 1;
 
-            Console.WriteLine(class2.MyProperty);
-            Console.WriteLine(class2.MyProperty1);
+            var inst = new Class4();
+            var i = inst.Prop1;
+            i = inst.Prop1;
+            i = inst.Prop1;
+            i = inst.Prop3;
+            i = inst.Prop3;
+            i = inst.Prop4;
+            i = inst.Prop4;
+            i = Class4.Prop2;
+            i = Class4.Prop2;
+
+            inst = new Class4();
+            i = inst.Prop1;
+
+            //Console.WriteLine(class2.MyProperty);
+            //Console.WriteLine(class2.MyProperty1);
             Console.ReadLine();
         }
     }
