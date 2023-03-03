70c518720b4a9a506c57aa4bfe4ff20467583eb6
Данил Сараев <leonardo147wot@gmail.com>
2023-03-03 13:57:24 +0300

    AsyncTests added

---
+++ Async/Async/AsyncTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Async
+{
+    internal static class AsyncTests
+    {
+        public static void Test1()
+        {
+            Console.WriteLine(nameof(Test1) + " started!");
+
+            AsyncMethod1().Wait();
+
+            Console.WriteLine(nameof(Test1) + " completed!");
+        }
+
+
+        private static async Task AsyncMethod1()
+        {
+            Console.WriteLine($"\nWaiting for {nameof(AsyncMethod1)}...");
+            await Task.Delay(1);
+            Console.WriteLine(nameof(AsyncMethod1) + " completed!");
+            Console.WriteLine();
+        }
+    }
+}
--- Async/Async/Program.cs
+++ Async/Async/Program.cs
@@ -1,4 +1,5 @@
-﻿using DS.ConsoleApp.MultithreadTest.CancelTests;
+﻿using Async;
+using DS.ConsoleApp.MultithreadTest.CancelTests;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,32 +11,19 @@ using System.Threading.Tasks;
 namespace DS.ConsoleApp.MultithreadTest
 {
     class Program
-    {
-        //DS.ConsoleApp.MultithreadTest static Task Main(string[] args)
-        //{
-        //    //HTMLLoader.Load();
-        //    await NamePrinter.PrintAwait();
-        //    //await NamePrinter.PrintAwait1();
-        //    //await NamePrinter.PrintNoAwait();
-        //    //await NamePrinter.PrintNameWithRun();
-        //    //NamePrinter.PrintNameWithRun1();
-
-        //    Console.ReadLine();
-        //}
-
-
-        static void Main0(string[] args)
+    {       
+        static void Main(string[] args)
         {
-            TaskTest.RunPrintNumbers();
-            //TaskTest.RunAndWaitMultipleTasks_InterLocked();
-            //ThreadsPrinter.RunTask();
-            //DeadLockTest.Run();
-            //CounterTest.Run();
+            Console.WriteLine("Main started.");
+
+            AsyncTests.Test1();
+
+            Console.WriteLine("Main completed.");
 
             Console.ReadLine();
         }
 
-        static async Task Main(string[] args)
+        static async Task AsyncMain(string[] args)
         {
             Console.WriteLine($"Поток {Thread.CurrentThread.ManagedThreadId} из Main запущен\n");
 
